d86c54149a36c2328492956971c1548c5a69a7a4
aimar <fanfu@DESKTOP-K965IRE>
2019-12-04 13:07:59 +0100

    User entity created.

--- src/java/serverapplication/entities/User.java
+++ src/java/serverapplication/entities/User.java
@@ -5,10 +5,230 @@
  */
 package serverapplication.entities;
 
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Collection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
- *
+ * This class is an entity.
  * @author aimar
  */
-public class User {
+@Entity
+@Table(name = "user", schema = "team6dbreto2")
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String login;
+    private String email;
+    private String fullName;
+    private Status status;
+    private Privilege privilege;
+    private String password;
+    private Timestamp lastAccess;
+    private Timestamp lastPasswordChange;
+    private Collection<Rating> ratings;
+    private Collection<Document> documents;
+    private Collection<Group> groups;
+    
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * @param login the login to set
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the fullName
+     */
+    public String getFullName() {
+        return fullName;
+    }
+
+    /**
+     * @param fullName the fullName to set
+     */
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    /**
+     * @return the status
+     */
+    public Status getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the privilege
+     */
+    public Privilege getPrivilege() {
+        return privilege;
+    }
+
+    /**
+     * @param privilege the privilege to set
+     */
+    public void setPrivilege(Privilege privilege) {
+        this.privilege = privilege;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the lastAccess
+     */
+    public Timestamp getLastAccess() {
+        return lastAccess;
+    }
+
+    /**
+     * @param lastAccess the lastAccess to set
+     */
+    public void setLastAccess(Timestamp lastAccess) {
+        this.lastAccess = lastAccess;
+    }
+
+    /**
+     * @return the lastPasswordChange
+     */
+    public Timestamp getLastPasswordChange() {
+        return lastPasswordChange;
+    }
+
+    /**
+     * @param lastPasswordChange the lastPasswordChange to set
+     */
+    public void setLastPasswordChange(Timestamp lastPasswordChange) {
+        this.lastPasswordChange = lastPasswordChange;
+    }
+
+    /**
+     * @return the ratings
+     */
+    public Collection<Rating> getRatings() {
+        return ratings;
+    }
+
+    /**
+     * @param ratings the ratings to set
+     */
+    public void setRatings(Collection<Rating> ratings) {
+        this.ratings = ratings;
+    }
+
+    /**
+     * @return the documents
+     */
+    public Collection<Document> getDocuments() {
+        return documents;
+    }
+
+    /**
+     * @param documents the documents to set
+     */
+    public void setDocuments(Collection<Document> documents) {
+        this.documents = documents;
+    }
+    
+    /**
+     * @return the groups
+     */
+    public Collection<Group> getGroups() {
+        return groups;
+    }
+
+    /**
+     * @param groups the groups to set
+     */
+    public void setGroups(Collection<Group> groups) {
+        this.groups = groups;
+    }
+    
+     @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof User)) {
+            return false;
+        }
+        User other = (User) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "serverapplication.entities.User[ id=" + id + " ]";
+    }
     
 }
