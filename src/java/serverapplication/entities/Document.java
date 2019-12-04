131a201fa57c475ef5d746ab1623343397ce5d12
jarsss8 <adriancgs@gmail.com>
2019-12-04 13:53:25 +0100

    Commit pre merge 2

--- src/java/serverapplication/entities/Document.java
+++ src/java/serverapplication/entities/Document.java
@@ -5,10 +5,160 @@
  */
 package serverapplication.entities;
 
+import java.io.Serializable;
+import java.sql.Blob;
+import java.sql.Timestamp;
+import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 /**
- *
- * @author aimar
+ * Entity class for Document.
+ * 
+ * @author Gaizka Andrés
  */
-public class Document {
+@Entity
+@Table(name="document",schema="team6dbreto2")
+public class Document implements Serializable{
+    private static final long serialVersionUID=1L;
+    /**
+     * Id to identificate the document
+     */
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    /**
+     * The name of the document
+     */
+    private String name;
+    /**
+     * The date when the document has been upload
+     */
+    private Timestamp uploadDate;
+    /**
+     * The total rating of the document
+     */
+    private int totalRating;
+    /**
+     * The total of reviews the document has
+     */
+    private int ratingCount;
+    /**
+     * The file itself
+     */
+    private Blob file;
+    /**
+     * The collection of rating the document has been given
+     */
+    private Set<Rating> ratings;
+    /**
+     * The author of the document
+     */
+    private User user;
     
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Timestamp getUploadDate() {
+        return uploadDate;
+    }
+
+    public void setUploadDate(Timestamp uploadDate) {
+        this.uploadDate = uploadDate;
+    }
+
+    public int getTotalRating() {
+        return totalRating;
+    }
+
+    public void setTotalRating(int totalRating) {
+        this.totalRating = totalRating;
+    }
+
+    public int getRatingAccount() {
+        return ratingCount;
+    }
+
+    public void setRatingAccount(int ratingAccount) {
+        this.ratingCount = ratingAccount;
+    }
+
+    public Blob getFile() {
+        return file;
+    }
+
+    public void setFile(Blob file) {
+        this.file = file;
+    }
+
+    public Set<Rating> getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(Set<Rating> ratings) {
+        this.ratings = ratings;
+    }
+
+    public User getUsers() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    /**
+     * Return an int calculated from id for the User
+     * @return an int representating the instance of this entity
+     */
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+    /**
+     * Compares two instances of Users
+     * @param object the other User instance to compare to
+     * @return true if instances are equal
+     */
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Document)) {
+            return false;
+        }
+        Document other = (Document) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+     /**
+     * Obtains a String representation including id value and classes full Name
+     * @return a String of an User id
+     */
+    @Override
+    public String toString() {
+        return "serverapplication.entities.Document[ id=" + id + " ]";
+    }
 }
--- src/java/serverapplication/entities/Group.java
+++ src/java/serverapplication/entities/Group.java
@@ -5,10 +5,163 @@
  */
 package serverapplication.entities;
 
+import java.io.Serializable;
+import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 /**
- *
- * @author aimar
+ * Entity of the groups of users
+ * @author Diego Urraca
  */
-public class Group {
+@Entity
+@Table(name="group",schema="team6dbreto2")
+public class Group implements Serializable{
+    private static final long serialVersionUID= 1L;
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String password;
+    private Long adminId;
+    //List of users that are in the group
+    @ManyToMany(mappedBy="group")
+    private Set<User> users;
+    //List of documents that are uploaded by the group
+    @ManyToOne
+    private Set<Document> documents;
+    /**
+     * @return the serialVersionUID
+     */
+    public static Long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the adminId
+     */
+    public long getAdminId() {
+        return adminId;
+    }
+
+    /**
+     * @param adminId the adminId to set
+     */
+    public void setAdminId(long adminId) {
+        this.adminId = adminId;
+    }
+
+    /**
+     * @return the users
+     */
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    /**
+     * @param users the users to set
+     */
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+    
+
+    /**
+     * @return the documents
+     */
+    public Set<Document> getDocuments() {
+        return documents;
+    }
+
+    /**
+     * @param documents the documents to set
+     */
+    public void setDocuments(Set<Document> documents) {
+        this.setDocuments(documents);
+    }
+    
+    /**
+     * Verify if an id is not null
+     * @return 0
+     */
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+    
+    /**
+     * Compare if two group ids are equals
+     * @param object
+     * @return true or false, it depends
+     */
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Group)) {
+            return false;
+        }
+        Group other = (Group) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+    
+    /**
+     * id to String
+     * @return the String
+     */
+    @Override
+    public String toString() {
+        return "serverapplication.entities.Group[ id=" + id + " ]";
+    }
     
 }
