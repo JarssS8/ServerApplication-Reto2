15ae416905e871c20bd61fff30eeee2a25d3a5be
jarsss8 <adriancgs@gmail.com>
2019-12-05 09:45:50 +0100

    Add coments to enumerations

--- src/java/serverapplication/entities/Status.java
+++ src/java/serverapplication/entities/Status.java
@@ -5,11 +5,20 @@
  */
 package serverapplication.entities;
 
+import java.io.Serializable;
+
 
 /**
  * Status enumeration for the user
  * @author Adrian
  */
-public enum Status{
-    ENABLED,DISABLED
+public enum Status implements Serializable{
+    /**
+     * If user is can access to the application is ENABLED.
+     */
+    ENABLED,
+    /**
+     * If user is can NOT access to the application is DISABLED.
+     */
+    DISABLED
 }
